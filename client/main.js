21e2111f334c585b89729122f0da9f464d97f8b4
eduardtarassov <eduardtarassov@gmail.com>
2016-10-04 00:03:10 +0100

    Dynamic template

--- client/main.js
+++ client/main.js
@@ -3,4 +3,14 @@ import angularMeteor from 'angular-meteor';
 
 angular.module('socially', [
     angularMeteor
-]);
+])
+.controller('PartiesListCtrl', function($scope) {
+    'ngInject';
+    $scope.parties = [{
+        'name': 'Dubstep-Free Zone',
+        'description': 'Can we please just for an evening not listen to dubstep.'
+    }, {
+        'name': 'All dubstep all the time',
+        'description': 'Not today!'
+    }];
+});
